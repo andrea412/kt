b8afadb950ae31de4a6739b97bc767d63f4b7ee2
andrea412 <76185362+andrea412@users.noreply.github.com>
2021-01-13 13:50:47 +0200

    Add files via upload

---
+++ KT-turtle.py
@@ -0,0 +1,113 @@
+#Andrea Magro IT-20
+#13.01.21
+#KT - turtle
+
+import turtle
+
+a = turtle.Screen()
+a.setup(500,500)
+a.title("ruudud - Andrea")
+
+n = 90
+k1 = turtle.Turtle()
+k1.color("blue")
+k1.fd(50)
+k1.left(90)
+k1.fd(50)
+k1.left(-90)
+k1.fd(50)
+k1.left(90)
+k1.fd(50)
+k1.left(90)
+k1.fd(50)
+k1.left(-90)
+k1.fd(50)
+k1.left(-90)
+k1.fd(-50)
+k1.left(90)
+k1.fd(-50)
+k1.left(-90)
+k1.fd(-50)
+k1.left(90)
+k1.fd(-50)
+k1.left(90)
+k1.fd(-50)
+k1.left(90)
+k1.fd(50)
+
+
+
+#kuues pilt
+# k1.color("red")
+# k1.fd(100)
+# k1.left(n)
+# k1.fd(30)
+# k1.left(n)
+# k1.fd(100)
+# k1.left(n)
+# k1.fd(30)
+# k1.left(n)
+# 
+# k3 = turtle.Turtle()
+# k3.color("blue")
+# k3.fd(-30)
+# k3.left(n)
+# k3.fd(100)
+# k3.left(n)
+# k3.fd(-30)
+# k3.left(n)
+# k3.fd(100)
+# 
+# k = turtle.Turtle()
+# k.color("orange")
+# k.fd(-100)
+# k.left(n)
+# k.fd(-30)
+# k.left(n)
+# k.fd(-100)
+# k.left(n)
+# k.fd(-30)
+# k.left(n)
+# 
+# k2 = turtle.Turtle()
+# k2.color("green")
+# k2.fd(30)
+# k2.left(n)
+# k2.fd(-100)
+# k2.left(n)
+# k2.fd(30)
+# k2.left(n)
+# k2.fd(-100)
+
+
+#kolmas pilt
+# k1 = turtle.Turtle()
+# k1.color("red")
+# k1.fd(100)
+# k1.left(90)
+# k1.fd(100)
+# k1.left(90)
+# k1.fd(100)
+# k1.left(90)
+# k1.fd(100)
+# k1.left(90)
+# 
+# k2 = turtle.Turtle()
+# k2.color("green")
+# k2.fd(50)
+# k2.left(90)
+# k2.fd(50)
+# k2.left(90)
+# k2.fd(100)
+# k2.left(90)
+# k2.fd(100)
+# k2.left(90)
+# k2.fd(100)
+# k2.left(90)
+# k2.fd(50)
+
+
+
+
+
+
